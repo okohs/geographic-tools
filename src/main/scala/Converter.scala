06ccbab6494f5fb5a3d7cc0865821f19176f0c8a
Kota Mizushima <mizukota@gmail.com>
2016-12-14 18:06:01 +0900

    Use += operator or updated method to add an entry to map
    Put only expression to return a value from a function.

--- src/main/scala/Converter.scala
+++ src/main/scala/Converter.scala
@@ -20,24 +20,23 @@ object Converter {
   }
 
   def convertGtoJ(params: Array[String]):Array[Double] = {
-    var result: Array[Double] = null
-    result = new Array[Double](2)
+    var result: Array[Double] = new Array[Double](2)
     val convertUrl = buildConverterUrl(params)
     val result = scala.io.Source.fromURL(convertUrl).mkString
   }
 
-  def buildConverterUrl(params:Array[String]):String = {
+  def buildConverterUrl(args: Array[String]):String = {
     val baseUrl = "http://vldb.gsi.go.jp/sokuchi/surveycalc/tky2jgd/tky2jgd.pl?"
     var params = Map('outputType -> "json", 'sokuti -> "1", 'Place -> "1", 'latitude -> "35.6730837", 'longitude -> "139.7599029")
 
     if (Array(0) == "gtoj") {
-      params('sokuti -> "2")
+      params += ('sokuti -> "2")
     }
-    params('latitude -> params(1))
-    params('longitude -> params(2))
+    params += ('latitude -> args(1))
+    params += ('longitude -> args(2))
 
     val paramsToString = params.map { case (key, value) => s"${key}=${value}" }.mkString("&")
 
-    val convertUrl = baseUrl + paramsToString
+    baseUrl + paramsToString
   }
 }
