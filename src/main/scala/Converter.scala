d8b343153688be7d9b03f7bfba850ed426108aa7
MATSUMURA Syoko <matsumura-s@gnavi.co.jp>
2016-12-29 14:41:41 +0900

    update value sample

--- src/main/scala/Converter.scala
+++ src/main/scala/Converter.scala
@@ -2,8 +2,8 @@ import play.api.libs.json._
 
 object Converter {
   def main(args: Array[String]): Unit = {
-    //    val args = Array("j2g","35.667153","139.779080")
-    //    val args = Array("g2j","35.6703917", "139.7758458")
+    //    val args = Array("j2g","35.667153", "139.779080")
+    //    val args = Array("g2j","35.670392", "139.775846")
     //    val args = Array("dms2deg", "139/41/30.3")
     //    val args = Array("deg2dms", "139.691749")
     if (args.length > 2 && (args(0) == "g2j" || args(0) == "j2g")) {
